7be14e4135d6dd0c389725281eb583c404b869d0
Lagatkipruto <kiprutowillie@gmail.com>
2023-06-06 16:24:34 +0000

    Advanced tasks and other mandatory tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,15 +5,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Set a custom User-Agent header to avoid errors"""
-    headers = {'User-Agent': 'MyBot/1.0'}
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A function that prints hot posts on a given Reddit subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the top 10 hottest posts on a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        }
+    params = {
+            "limit": 10
+            }
+    response = requests.get(url, headers=headers, params=params,
+            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return 
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
