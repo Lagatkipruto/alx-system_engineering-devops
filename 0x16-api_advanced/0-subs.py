dd76b640e70bc503d42ec87897612f667e9c299c
Wilfred Kipruto Lagat <kiprutowillie@gmail.com>
2023-06-06 09:40:56 -0700

    Fixing pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of subscribers"""
+"""Function that queries the Reddit API
+and returns the number of subscribers"""
 import requests
 
 
