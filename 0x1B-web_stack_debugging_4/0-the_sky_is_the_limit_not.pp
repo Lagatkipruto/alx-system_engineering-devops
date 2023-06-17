3157ed8a3e300ca7e0387f0b1d15e081cfbc5745
Lagatkipruto <kiprutowillie@gmail.com>
2023-06-17 09:22:01 +0000

    Fixing puppet-lint on first task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,7 +5,6 @@ exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
-
 # Restart Nginx
   exec { 'nginx-restart':
   command => '/etc/init.d/nginx restart',
